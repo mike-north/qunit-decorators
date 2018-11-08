42a576e0568119a355715b0803f74e15dd6c53d9
Mike North <michael.l.north@gmail.com>
2018-11-07 18:04:58 -0800

    chore: failing test for #35

--- test/context-test.ts
+++ test/context-test.ts
@@ -7,9 +7,16 @@ import { suite, test } from 'qunit-decorators';
 })
 class ContextTests {
   foo = 'valueOfFoo';
+
+  capitalize(raw: string) {
+    if (raw.length < 2) return raw.toUpperCase();
+    return [raw[0].toUpperCase(), raw.substr(1)].join('');
+  }
+
   @test
   memberDataOnClass(a: Assert) {
     a.equal(this.foo, 'valueOfFoo');
+    a.equal(this.capitalize(this.foo), 'ValueOfFoo');
   }
 
   @test
